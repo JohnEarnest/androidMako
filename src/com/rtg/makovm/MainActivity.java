86db34fce1498e7b6f2779db1f1feae771f63940
Chris S <gummybassist@gmail.com>
2012-12-06 00:16:54 +0000

    OPTIMISED file loading and added keyUp and keyDown methods- yet to be implemented

--- src/com/rtg/makovm/MainActivity.java
+++ src/com/rtg/makovm/MainActivity.java
@@ -2,14 +2,34 @@ package com.rtg.makovm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {
+	private MakoView mMakoView = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// Retrieve a reference to the view inflated in the layout
+		mMakoView = (MakoView) findViewById(R.id.MainActivity_MakoView);
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		mMakoView.onKeyUp(keyCode, event);
+		return super.onKeyUp(keyCode, event);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		mMakoView.onKeyDown(keyCode, event);
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public void onBackPressed() {
+		return;
+	}
 }
--- src/com/rtg/makovm/MakoView.java
+++ src/com/rtg/makovm/MakoView.java
@@ -1,7 +1,10 @@
 package com.rtg.makovm;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
+import android.view.KeyEvent;
 import android.view.View;
 import android.content.Context;
 import android.util.AttributeSet;
@@ -10,7 +13,8 @@ import android.graphics.*;
 
 public class MakoView extends View {
 
-	MakoVM vm;
+	private static final int FRAME_RATE = 1000/60;
+	private MakoVM vm;
 
 	public MakoView(Context c, AttributeSet a) {
 		super(c, a);
@@ -27,12 +31,27 @@ public class MakoView extends View {
 
 	private static int[] loadRom(InputStream i, int[] prev) {
 		try {
-			DataInputStream in = new DataInputStream(i);
-			int[] rom = new int[in.available() / 4];
-			for(int x = 0; x < rom.length; x++) {
-				rom[x] = in.readInt();
+			int romSize = i.available();
+			
+			// Allocate a byteBuffer, this will be used later to convert the bytes
+			// into an integer array
+			ByteBuffer buffer = ByteBuffer.allocate(romSize);
+			buffer.clear();
+			byte[] page = new byte[4096];
+			int read = 0;
+			int totalRead = 0;
+			while(i.available()>0)
+			{
+				read = i.read(page, 0, 4096);
+				totalRead += read;
+				buffer.put(page,0, read);
 			}
-			in.close();
+			int[] rom = new int[totalRead/4];
+			buffer.rewind();
+			IntBuffer intBuf = buffer.asIntBuffer();
+			intBuf.get(rom);
+			buffer.clear();
+			i.close();
 			System.out.println("Restored from save file!");
 			return rom;
 		}
@@ -41,10 +60,26 @@ public class MakoView extends View {
 			return prev;
 		}
 	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		return super.onKeyUp(keyCode, event);
+	}
 
 	@Override
 	public void onDraw(Canvas c) {
 		super.onDraw(c);
+		c.save();
+		c.scale(2.5f, 2.5f);
 		c.drawBitmap(vm.p, 0, 320, 0, 0, 320, 240, false, null);
+		c.restore();
+		
+		vm.run();
+		postInvalidateDelayed(FRAME_RATE);
 	}
 }
